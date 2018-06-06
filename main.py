723b1380105d1b322b423d0ad96f431e587c84bc
Vitalik <39953100+sleepwallker@users.noreply.github.com>
2018-06-06 18:18:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ import os.path
 import requests
 
 
+
 class MainHandler(tornado.web.RequestHandler):
 
     def post(self):
         self.set_header("Content-Type", "text/plain")
         self.write("You wrote " + self.request.body())
+
+    def pretty_print_POST(req):
+        """
+        At this point it is completely built and ready
+        to be fired; it is "prepared".
+
+        However pay attention at the formatting used in
+        this function because it is programmed to be pretty
+        printed and may differ from the actual request.
+        """
         print('{}\n{}\n{}\n\n{}'.format(
+            '-----------START-----------',
             req.method + ' ' + req.url,
             '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
             req.body,
         ))
+
+ 
 
 
 
